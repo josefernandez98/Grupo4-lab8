86636406bc5ae70edcccc3ed4953a3b02c04ea2e
Harold Mendoza <hrmendoza96@unitec.edu>
2016-12-09 13:20:06 -0600

    Commit H

--- Soldado.cpp
+++ Soldado.cpp
@@ -1,33 +1,38 @@
 #include <iostream>
+#include "Soldado.h"
 using namespace std;
 
 Soldado::Soldado(){
 
 }
-Soldado::Soldado(string, string, int){
+Soldado::Soldado(string nombre, string ciudad, int edad){
+
 
 }
-void Soldado::setNombre(string){
+void Soldado::setNombre(string nombre){
 
 }
-void Soldado::setCiudad(string){
+void Soldado::setCiudad(string ciudad){
 
 }
-void Soldado::setEdad(int){
+void Soldado::setEdad(int edad){
 
 }
 string Soldado::getNombre(){
+  return nombre;
 
 }
 string Soldado::getCiudad(){
+  return ciudad;
 
 }
 int Soldado::getEdad(){
+  return edad;
 
 }
 string Soldado::toString(){
 
 }
 Soldado::~Soldado(){
-  
+
 }
