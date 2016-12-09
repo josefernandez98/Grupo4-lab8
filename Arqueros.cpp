ac86341eb37a132b24899d077ea62d99f3746119
Jose Fernandez <jrfo_04@unitec.edu>
2016-12-09 13:27:53 -0600

    stfu ana

--- Arqueros.cpp
+++ Arqueros.cpp
@@ -1,2 +1,4 @@
 #include <iostream>
 #include "Soldado.h"
+#include <string>
+using namespace std;
--- Arqueros.h
+++ Arqueros.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <iostream>
 #include "Soldado.h"
+#include <string>
 
 using namespace std;
 
