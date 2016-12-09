ea056ffbbe629bdbd654be93a57d6b4a2308a46b
Harold Mendoza <hrmendoza96@unitec.edu>
2016-12-09 14:27:09 -0600

    Commit H

--- Main.cpp
+++ Main.cpp
@@ -5,29 +5,24 @@ using namespace std;
 void menu();
 
 int main(int argc, char const *argv[]) {
-    initscr();
-    raw();
-    int opcion = 0;
-    char opcionCH = opcion - '0';
-    do {
-        menu();
-        opcionCH = getch();
-        if(opcionCH =='1'){
+  initscr();
+  raw();
+  int opcion = 0;
+  char opcionCH = opcion - '0';
+  do {
+    menu();
+    opcionCH = getch();
+    if(opcion==1){
 
-        }//Fin opcion 1
-        if(opcionCH == '2'){
+    }
+    if(opcion==2){
 
-<<<<<<< HEAD
     }
     if(opcion==3){
       
-=======
-        }//Fin opcion 2
-        if(opcionCH == '3'){
->>>>>>> 7b8ccb2244d970d5cbb8f0ab82d7ed57692ba13d
 
-        }//Fin opcion 3
-    } while(opcionCH!=3);
+    }
+  } while(opcion!=3);
 
 
 
@@ -45,5 +40,5 @@ void menu(){
   printw("1) Crear Escuadron");
   printw("2) Simulacion");
   printw("3) Salir");
-  printw("Ingrese su opcion:");
-}//Fin del menu
+
+}
