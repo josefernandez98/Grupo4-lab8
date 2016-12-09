d278516b86238e6a07ca9fac65bf408edeee4486
Harold Mendoza <hrmendoza96@unitec.edu>
2016-12-09 13:17:46 -0600

    Commit H

---
+++ Soldado.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+Soldado::Soldado(){
+
+}
+Soldado::Soldado(string, string, int){
+
+}
+void Soldado::setNombre(string){
+
+}
+void Soldado::setCiudad(string){
+
+}
+void Soldado::setEdad(int){
+
+}
+string Soldado::getNombre(){
+
+}
+string Soldado::getCiudad(){
+
+}
+int Soldado::getEdad(){
+
+}
+string Soldado::toString(){
+
+}
+Soldado::~Soldado(){
+  
+}
---
+++ Soldado.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Soldado{
+  protected:
+    string nombre;
+    string ciudad;
+    int edad;
+
+  private:
+    Soldado();
+    Soldado(string, string, int);
+    void setNombre(string);
+    void setCiudad(string);
+    void setEdad(int);
+    string getNombre();
+    string getCiudad();
+    int getEdad();
+    string toString();
+    ~Soldado();
+};
